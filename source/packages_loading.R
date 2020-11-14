a65c31316bbe5ce87f444435a5c13eab7c2d8523
Giacomo Frisoni <giacomofrisoni@gmail.com>
2020-11-14 14:00:20 +0100

    Added code for required package loading

---
+++ source/packages_loading.R
@@ -0,0 +1,73 @@
+##########################################
+## Loading R packages
+##########################################
+
+# Package for R and Python integration
+library(reticulate)
+
+# Package for progress bar
+library(progress)
+
+# Package for working with data frame
+library(plyr)
+library(dplyr)
+
+# Package for string manipulation
+# (e.g. replacement of occurrences of a matched pattern in a string)
+library(stringr)
+
+# Package for unlist with depth specification
+library(NCmisc)
+
+# Package for multiple slang substitution
+library(textclean)
+
+# Package for functional programming toolkit
+library(purrr)
+
+# Package for plotting charts
+library(ggplot2)
+
+# Package for combine multiple ggplot charts (wraps cowplot package)
+library(ggpubr)
+
+# Package for data frame sorting
+library(doBy)
+
+# Package for Text Mining utilities
+# Includes preprocessing operations similar to StringToWordVector in Weka and Corpus handling
+# (content transformation, stopwords removal, single words extraction, common features selection etc.)
+library(tm)
+
+# Package for quantitative text analysis
+library(quanteda)
+
+# Package for LSA analysis
+library(lsa)
+
+# Package for Fast Randomized Singular Value Decomposition
+library(rsvd)
+
+# Package for t-SNE
+library(Rtsne)
+
+# Package for iteration on permutations, combinations etc. of list items
+library(arrangements)
+
+# Package for date filtering
+library(lubridate)
+
+# Package for similarity / dissimilarity measures for 'dist()'
+library(proxy)
+
+# Package for dendrogram editing
+library(dendextend)
+
+# Package for hierarchical clustering eith bootstrapped p-values
+library(pvclust)
+
+# Package used for correlation matrix reshaping and heatmap creation
+library(reshape2)
+
+# Package for Locality Sensitive Hashing (LSH)
+library(LSHR)
